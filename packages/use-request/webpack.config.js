a13827454f39fbae83461b6bbf0e1d253ca35518
Libing sun <slbing@maplecloudy.com>
2021-08-27 18:21:58 +0800

    modify export js name

--- packages/use-request/webpack.config.js
+++ packages/use-request/webpack.config.js
@@ -5,8 +5,8 @@ const path = require('path');
 module.exports = merge(common, {
   entry: './es/index.js',
   output: {
-    filename: 'ahooks-use-request.js',
-    library: 'ahooksUseRequest',
+    filename: 'qhooks-use-request.js',
+    library: 'qhooksUseRequest',
     path: path.resolve(__dirname, './dist'),
   },
 });
